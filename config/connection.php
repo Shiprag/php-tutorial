91e9bc34b4d50c5afdf900a8821a8f9b779f1649
Mohit Gupta <mohit@mogify.com>
2014-07-31 20:52:25 -0400

    Heroku changes

--- config/connection.php
+++ config/connection.php
@@ -1,6 +1,13 @@
 <?php
 
 //database connection here....
-$dbc=mysqli_connect('localhost','root','shipramohit','mogify') OR die('Error: '.mysqli_connect_error);
+$url=parse_url(getenv("CLEARDB_DATABASE_URL"));
 
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"],1);
+
+//$dbc=mysqli_connect('localhost','root','shipramohit','mogify') OR die('Error: '.mysqli_connect_error);
+  $dbc = mysqli_connect($server, $username, $password) OR die('Error: '.mysqli_connect_error);
 ?>
